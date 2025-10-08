3a52a4a49813f4c548d8fb85768498fcd509f45d
Christopher <nebomusic@users.noreply.github.com>
2025-10-08 17:42:27 -0400

    Added PID Settings for Single Launch Wheel Motor

--- TeleopStrafer_Quad_2025.java
+++ TeleopStrafer_Quad_2025.java
@@ -6,11 +6,19 @@ All rights reserved.
 Modified by michaudc 2017
 Additional modifications by michaudc 2021
 Modifications for CENTERSTAGE by michaudc 2023
-Modifiations for DECODE by michaudc 2025
 */
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import java.util.concurrent.TimeUnit;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -18,6 +26,9 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * This file provides basic Telop driving for a Pushbot robot.
@@ -43,10 +54,48 @@ public class TeleopStrafer_Quad_2025 extends OpMode {
                                                          // could also use HardwarePushbotMatrix class.
     GoBildaPinpointDriver odo;
     
-    // Values based on DcMotorEx and .setVelocity()    
-    private double SHOOTER_FULL = -1260;
-    private double SHOOTER_HALF = -600;
+    double          clawOffset  = 0.0 ;                  // Servo mid position
+    final double    CLAW_SPEED  = 0.02 ;                 // sets rate to move servo
+    
+    int armPos = 0;
+    int pastPos = 0;
+    int deltaPos = armPos - pastPos;
+    
+    private double ARM_SPEED = 0.7;
+    private double DRIVE_SPEED = 0.5;
+
+    private double ClIMB_POWER = 0.8;
+    
+    private double SHOOTER_FULL = -1520;
+    private double SHOOTER_HALF = -1420;
     private double SHOOTER_VELOCITY = 0;
+    
+    private double NEW_P = 160;
+    private double NEW_I = 1.5;
+    private double NEW_D = 0;
+    
+    
+    // for Camera
+    // Adjust these numbers to suit your robot.
+    final double DESIRED_DISTANCE = 55.0; //  this is how close the camera should get to the target (inches)
+
+    //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
+    //  applied to the drive motors to correct the error.
+    //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
+    final double SPEED_GAIN  =  0.02  ;   //  Forward Speed Control "Gain". e.g. Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+    final double STRAFE_GAIN =  0.015 ;   //  Strafe Speed Control "Gain".  e.g. Ramp up to 37% power at a 25 degree Yaw error.   (0.375 / 25.0)
+    final double TURN_GAIN   =  0.01  ;   //  Turn Control "Gain".  e.g. Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+
+    final double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+    final double MAX_AUTO_STRAFE= 0.5;   //  Clip the strafing speed to this max value (adjust for your robot)
+    final double MAX_AUTO_TURN  = 0.3;   //  Clip the turn speed to this max value (adjust for your robot)
+
+    private static final boolean USE_WEBCAM = true;  // Set true to use a webcam, or false for a phone camera
+    private static final int DESIRED_TAG_ID = -1;     // Choose the tag you want to approach or set to -1 for ANY tag.
+    private VisionPortal visionPortal;               // Used to manage the video source.
+    private AprilTagProcessor aprilTag;              // Used for managing the AprilTag detection process.
+    private AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
+
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -57,10 +106,16 @@ public class TeleopStrafer_Quad_2025 extends OpMode {
          * The init() method of the hardware class does all the work here
          */
         robot.init(hardwareMap);
-
-        // Initialize the GoBilda Odometry Computer
         odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
         
+        boolean targetFound     = false;    // Set to true when an AprilTag target is detected
+        double  drive           = 0;        // Desired forward power/speed (-1 to +1)
+        double  strafe          = 0;        // Desired strafe power/speed (-1 to +1)
+        double  turn            = 0;        // Desired turning power/speed (-1 to +1)
+
+        // Initialize the Apriltag Detection process
+        initAprilTag();
+
         /*
         Set the odometry pod positions relative to the point that the odometry computer tracks around.
         The X pod offset refers to how far sideways from the tracking point the
@@ -98,6 +153,9 @@ public class TeleopStrafer_Quad_2025 extends OpMode {
          */
         //odo.recalibrateIMU();
         odo.resetPosAndIMU();
+        
+
+        
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Say", "Robot Ready");    //
@@ -110,7 +168,20 @@ public class TeleopStrafer_Quad_2025 extends OpMode {
         
         //robot.leftArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.leftArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.rightArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);        
+        armPos = robot.leftArm.getCurrentPosition();
+        
+        // Set PID
+        PIDCoefficients pidSettings = new PIDCoefficients(NEW_P, NEW_I, NEW_D);
+        robot.leftArm.setPIDCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidSettings);
+        
+        if (USE_WEBCAM)
+            setManualExposure(6, 250);  // Use low exposure time to reduce motion blur
+
+        // Wait for driver to press start
+        telemetry.addData("Camera preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch START to start OpMode");
+        telemetry.update();
+        
 
     }
 
@@ -141,11 +212,11 @@ public class TeleopStrafer_Quad_2025 extends OpMode {
          */
         odo.update();
         
-        // Variables for Joystick Paddle input
         double leftX;
         double leftY;
         double rightX;
         double rightY;
+        boolean targetFound     = false;    // Set to true when an AprilTag target is detected
 
         // Strafer Mode
         leftX = gamepad1.left_stick_x;
@@ -153,25 +224,87 @@ public class TeleopStrafer_Quad_2025 extends OpMode {
         rightX = gamepad1.right_stick_x ;
         rightY = gamepad1.right_stick_y;
         
-        // For Field Centric
         Pose2D pose = odo.getPosition();
         double heading = pose.getHeading(AngleUnit.DEGREES);
         //robot.driveFieldCentric(leftX, leftY, rightX, heading);
         
-        // Reset Odometry Computer Heading and Location
+        targetFound = false;
+        desiredTag  = null;
+
+        // Step through the list of detected tags and look for a matching tag
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        for (AprilTagDetection detection : currentDetections) {
+            // Look to see if we have size info on this tag.
+            if (detection.metadata != null) {
+                //  Check to see if we want to track towards this tag.
+                if ((DESIRED_TAG_ID < 0) || (detection.id == DESIRED_TAG_ID)) {
+                    // Yes, we want to use this tag.
+                    targetFound = true;
+                    desiredTag = detection;
+                    break;  // don't look any further.
+                } else {
+                    // This tag is in the library, but we do not want to track it right now.
+                    telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+                }
+            } else {
+                // This tag is NOT in the library, so we don't have enough information to track to it.
+                telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+            }
+        }
+
+        // Tell the driver what we see, and what to do.
+        if (targetFound) {
+            //telemetry.addData("\n>","HOLD Left-Bumper to Drive to Target\n");
+            //telemetry.addData("Found", "ID %d (%s)", desiredTag.id, desiredTag.metadata.name);
+            //telemetry.addData("Range",  "%5.1f inches", desiredTag.ftcPose.range);
+            //telemetry.addData("Bearing","%3.0f degrees", desiredTag.ftcPose.bearing);
+            //telemetry.addData("Yaw","%3.0f degrees", desiredTag.ftcPose.yaw);
+        } else {
+            //telemetry.addData("\n>","Drive using joysticks to find valid target\n");
+        }
+
+        // If Left Bumper is being pressed, AND we have found the desired target, Drive to target Automatically .
+        if (gamepad1.left_bumper && targetFound) {
+
+            // Determine heading, range and Yaw (tag image rotation) error so we can use them to control the robot automatically.
+            double  rangeError      = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
+            double  headingError    = desiredTag.ftcPose.bearing;
+            double  yawError        = desiredTag.ftcPose.yaw;
+
+            // Use the speed and turn "gains" to calculate how we want the robot to move.
+            leftY  = -Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+            rightX   = -Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
+            leftX = -Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+
+            //telemetry.addData("Auto","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
+        } else {
+
+            // drive using manual POV Joystick mode.  Slow things down to make the robot more controlable.
+            leftX = gamepad1.left_stick_x;
+            leftY = gamepad1.left_stick_y;
+            rightX = gamepad1.right_stick_x ;
+        }
+        telemetry.update();
+
+        // Apply desired axes motions to the drivetrain.
+        robot.driveStrafer(leftX, leftY, rightX);
+        //robot.driveTankTwoWheel(leftY, rightY);
+        //sleep(10);
+    
+        
         if (gamepad1.a) {
             odo.resetPosAndIMU();
         }
+
         
-        // For Robot Centric
-        robot.driveStrafer(leftX, leftY, rightX);
-        
-        // Launcher Value Settings based on dpad
+        // Shooter
         if (gamepad1.dpad_up) {
+            //robot.leftArm.setVelocity(SHOOTER_FULL);
             SHOOTER_VELOCITY = SHOOTER_FULL;
         }
         if (gamepad1.dpad_left) {
-            SHOOTER_VELOCITY += 1;
+            //SHOOTER_VELOCITY += 1;
+            SHOOTER_VELOCITY = SHOOTER_HALF;
         }
         if (gamepad1.dpad_right) {
             SHOOTER_VELOCITY -= 1;
@@ -179,19 +312,19 @@ public class TeleopStrafer_Quad_2025 extends OpMode {
         if (gamepad1.dpad_down) {
             SHOOTER_VELOCITY = 0;
         }
-        // Range Checking - leftArm should not go backwards
+        
         if (SHOOTER_VELOCITY > 0) {
             SHOOTER_VELOCITY = 0;
         }
-        // Set the leftArm to the proper velocity
+        
         robot.leftArm.setVelocity(SHOOTER_VELOCITY);
         
-        // Feeder Code - Continous Rotation Servos
+        // Feeder
         if (gamepad1.left_trigger > 0.5) {
             robot.leftHand.setPosition(1);
             robot.rightHand.setPosition(0);
         }
-        else if (gamepad1.right_trigger > 0.5) {
+        else if (gamepad1.right_trigger > 0.5) {//  && robot.leftArm.getVelocity() < -1510 && robot.leftArm.getVelocity() > -1550) {
             robot.leftHand.setPosition(0);
             robot.rightHand.setPosition(1);
         }
@@ -199,11 +332,14 @@ public class TeleopStrafer_Quad_2025 extends OpMode {
             robot.leftHand.setPosition(0.5);
             robot.rightHand.setPosition(0.5);
         }
-
-        // Intake Code
-        //TODO: Finish This
         
-        // Diagnostic Outputs - Velocity of Launcher Motor (leftArm)
+        //pastPos = armPos;
+        
+        // Get PID Information
+        PIDCoefficients pidValues = robot.leftArm.getPIDCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
+        telemetry.addData("PID Values", "%.04f, %.04f, %.04f", pidValues.p, pidValues.i, pidValues.d);
+        
+        //telemetry.addData("delta", deltaPos);
         double velocity = robot.leftArm.getVelocity();
         telemetry.addData("Velocity", velocity);
         telemetry.update();
@@ -215,6 +351,74 @@ public class TeleopStrafer_Quad_2025 extends OpMode {
      */
     @Override
     public void stop() {
+    }
+    
+    /**
+     * Initialize the AprilTag processor.
+     */
+    private void initAprilTag() {
+        // Create the AprilTag processor by using a builder.
+        aprilTag = new AprilTagProcessor.Builder().build();
+
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // e.g. Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        aprilTag.setDecimation(2);
+
+        // Create the vision portal by using a builder.
+        if (USE_WEBCAM) {
+            visionPortal = new VisionPortal.Builder()
+                    .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                    .addProcessor(aprilTag)
+                    .build();
+        } else {
+            visionPortal = new VisionPortal.Builder()
+                    .setCamera(BuiltinCameraDirection.BACK)
+                    .addProcessor(aprilTag)
+                    .build();
+        }
+    }
+
+    /*
+     Manually set the camera gain and exposure.
+     This can only be called AFTER calling initAprilTag(), and only works for Webcams;
+    */
+    private void    setManualExposure(int exposureMS, int gain) {
+        // Wait for the camera to be open, then use the controls
+
+        if (visionPortal == null) {
+            return;
+        }
+
+        // Make sure camera is streaming before we try to set the exposure controls
+        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            telemetry.addData("Camera", "Waiting");
+            telemetry.update();
+            while ((visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+                //sleep(20);
+            }
+            telemetry.addData("Camera", "Ready");
+            telemetry.update();
+        }
+
+        // Set camera controls unless we are stopping.
+        if (false)
+        {
+            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+                exposureControl.setMode(ExposureControl.Mode.Manual);
+                //sleep(50);
+            }
+            exposureControl.setExposure((long)exposureMS, TimeUnit.MILLISECONDS);
+            //sleep(20);
+            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+            gainControl.setGain(gain);
+            //sleep(20);
+        }
     }
 
 }
